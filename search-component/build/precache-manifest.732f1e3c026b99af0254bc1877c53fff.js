61c55687b309da34c5e4a3d74ee34c04a6efea30
Rutwik Waghmare <rutwik18012001@gmail.com>
2023-08-04 01:08:57 +0530

    build

---
+++ search-component/build/precache-manifest.732f1e3c026b99af0254bc1877c53fff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cef5b201314a024a624e",
+    "url": "/static/css/main.67a700c5.chunk.css"
+  },
+  {
+    "revision": "cef5b201314a024a624e",
+    "url": "/static/js/main.cef5b201.chunk.js"
+  },
+  {
+    "revision": "58294a3d856a0542909d",
+    "url": "/static/js/1.58294a3d.chunk.js"
+  },
+  {
+    "revision": "ab04901860f9ca2c33ba",
+    "url": "/static/js/2.ab049018.chunk.js"
+  },
+  {
+    "revision": "4594604607cbe964e03e",
+    "url": "/static/js/runtime~main.45946046.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "bca72fd573f007754f04035231c74edb",
+    "url": "/index.html"
+  }
+];
